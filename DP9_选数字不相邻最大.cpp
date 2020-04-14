3cbfef808ef6edb982c0a016b132e4fff4961cfb
zhourongtu <zhourongtu@qq.com>
2020-04-14 22:39:55 +0800

    v14 1.写了关于选不相邻数字达到最大和的题目。 2.对其进行了简单的状态压缩。 3.这道题和DP8基本一致，采用思想为选与不选。

---
+++ DP9_选数字不相邻最大.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+class Solution{
+public:
+int choose_maxSum(std::vector<int> &nums){
+    using std::vector;
+    int N = nums.size();
+    if(N == 1){
+        return nums[0];
+    }
+    // vector<int> opt(N);
+    // opt[0] = nums[0]; opt[1] = max(nums[0], nums[1]);
+    // for(int i=2; i<N; i++){
+    //     opt[i] = max(opt[i-1], opt[i-2]+nums[i]);
+    // }
+    // return opt[N-1];
+    
+    //状态压缩
+    int opt1, opt2, opt3;
+    opt1 = nums[0];
+    opt2 = max(nums[0], nums[1]);
+    for(int i=2; i<N; i++){
+        opt3 = max(opt2, opt1 + nums[i]);
+        opt1 = opt2;
+        opt2 = opt3;
+    }
+    return opt2;
+};
+
+};
+int main()
+{
+    Solution a;
+    vector<int> nums = {1, 2, 4, 1, 7, 8, 3};
+    cout << a.choose_maxSum(nums);
+}
