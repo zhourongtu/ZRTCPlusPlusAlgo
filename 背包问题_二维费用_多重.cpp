b73d759df9f8a09fde59e48e7f83b9d7ef4e9d3d
zhourongtu <zhourongtu@qq.com>
2020-08-01 16:41:43 +0800

    添加了背包问题的二位费用情况。都采用了一定的优化，前k件物品。代码中都是逆序

---
+++ 背包问题_二维费用_多重.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+/*
+ * DP[i][j][k]定义为，在前i个物品的条件下，拥有体积j，重量承受能力k的背包，最多能获取多少价值的物品
+*/
+class Solution{
+public:
+    MaxPrice_Multi(int N, int V, int W, vector<int> &V_cost, vector<int> &W_cost, vector<int> &Price, vector<int> Count)
+    {
+        vector<vector<int>> dp(V+1, vector<int>(W+1, 0));
+        // 逆序状态压缩
+        for(int k=1; k<=N; k++){// 第k件物品
+            for(int i=V; i>=V_cost[k]; i--){
+                for(int j=W; j>=W_cost[k]; j--){
+                    int max_start = min(i/V_cost[k], j/W_cost[k]);
+                    max_start = min(max_start, Count[k]);
+                    for(int V_k=max_start, W_k=max_start; V_k>=1; V_k--, W_k--){
+                        dp[i][j] = max(dp[i][j], dp[i-V_k*V_cost[k]][j-W_k*W_cost[k]] + V_k*Price[k]);
+                    }
+                }
+            }
+        }
+        return dp[V][W];
+    }
+};
+int main()
+{
+    int N, V, W; // V是容量，W是质量
+    N = 4; V = 5; W = 6;
+    vector<int> V_cost = {0, 1, 2, 3, 4};
+    vector<int> W_cost = {0, 2, 4, 4, 5};
+    vector<int> Count = {0, 3, 1, 1, 1};
+    vector<int> Price = {0, 3, 4, 5, 6};
+    Solution s;
+    cout << s.MaxPrice_Multi(N, V, W, V_cost, W_cost, Price, Count);
+    return 0;
+}
