a842b15285c111d534aa3d653db31b252a718692
zhourongtu <zhourongtu@qq.com>
2020-08-01 13:11:32 +0800

    添加了单调队列，烽火台的两种存值方式。都使用了单调队列

--- 单调队列_烽火台_存下标.cpp
+++ 单调队列_烽火台_存下标.cpp
@@ -11,33 +11,38 @@ using namespace std;
 class Solution{
 public:
     // dp方法
-    // 状态转移。dp = min
+    // 状态转移。dp[i] = min(dp[i-M] ~ dp[i-1]) + cost[i];
     int BeaconTower(int N, int M, vector<int> cost)
     {// N为总烽火台数，M为每M个烽火台需要一个点亮，cost代表点亮烽火台需要的代价
     // +0保证当前
         vector<int> f(N+1, 0);
         // 前面M-1个点亮自己的代价，都是自己（其实第M个也是）
         deque<int> single_queue;
+        
         for(int i=1; i<=M; ++i){
             f[i] = cost[i];
-            // single_queue.back()
-            while(!single_queue.empty() && single_queue.back() > f[i]){
+            while(!single_queue.empty() && f[single_queue.back()] >= f[i]){
                 single_queue.pop_back();
             }
-            single_queue.push_back(f[i]);
+            single_queue.push_back(i);
         }
-        for(int i=M+1; i<=N; ++i){
+        // 从第M个开始，（设为i）
+        // 代价为 i-M~i-1（因为自己必定点亮）中最小代价+自己代价
+        for(int i=M+1; i<=N; ++i){ 
             f[i] = 0x7FFFFFFF;
-            f[i] = single_queue.front() + cost[i]; // 自己的最小代价
-            while(!single_queue.empty() && single_queue.back() > f[i]){ 
+            f[i] = f[single_queue.front()] + cost[i]; // 自己的最小代价
+            while(!single_queue.empty() && f[single_queue.back()] >= f[i]){ 
                 single_queue.pop_back();
             }
-            if(!single_queue.empty() &&  f[i-M] == single_queue.front()){
+            single_queue.push_back(i);
+            /*
+                这里如果在single_queue中有多个值，更大的值如果在前面的下标，此时没有被pop掉
+            */
+            // 实际上可以用if
+            while(!single_queue.empty() && single_queue.front() < i-M+1) {
                 single_queue.pop_front();
             }
-            single_queue.push_back(f[i]);
         }
-
         // 统计N-M+1 ~ N的代价
         int ans = 0x7FFFFFFF;
         for(int i = N-M+1; i<=N; ++i){
@@ -64,4 +69,3 @@ int main()
     cout << s.BeaconTower(N, M, cost);
     return 0;
 }
-
