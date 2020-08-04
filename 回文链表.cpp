c816980ed634b2564ac317a5be61fe181de73348
zhourongtu <zhourongtu@qq.com>
2020-08-04 20:17:57 +0800

    添加了回文链表的解决方法（代码还未实现）

---
+++ 回文链表.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+/*
+ * 判断一个链表是不是回文。
+ * 方法1：翻转链表，判断是否相同。
+ * 方法2：翻转一半的链表，判断是否相同。
+ * 方法3：先获取链表长度，然后进行stack判断。
+*/
+class Solution{
+public:
+};
+int main()
+{
+    
+    return 0;
+}
