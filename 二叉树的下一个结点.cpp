90530090089384cdf02d9d6493d4ea6c97d3732b
zhourongtu <zhourongtu@qq.com>
2020-08-16 15:55:04 +0800

    添加了另一种实现方法，分类讨论。

--- 二叉树的下一个结点.cpp
+++ 二叉树的下一个结点.cpp
@@ -57,3 +57,43 @@ public:
 		return NULL;
     }
 };
+
+
+/*
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+        
+    }
+};
+*/
+// 这道题本质上是构建一个
+
+class Solution {
+public:
+    
+    
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        // 找到这个结点以后，开始思考有哪几种情况
+        // 1.有右子树-->查找右子树的最小值
+        if(pNode->right){
+            TreeLinkNode *temp = pNode->right;
+            while(temp->left){
+                temp = temp->left;
+            }
+            return temp;
+        }
+        while(pNode->next){
+            TreeLinkNode *root = pNode->next;
+            if(root->left == pNode){
+                return root;
+            }
+            pNode = pNode->next;
+        }
+        return NULL;
+    }
+};
