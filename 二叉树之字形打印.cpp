5ef4efc73a316966affcbe4963b5db6f585bbb44
zhourongtu <zhourongtu@qq.com>
2020-08-16 19:23:44 +0800

    添加了二叉树之字形打印的第二种方法，通过计数来判断

--- 二叉树之字形打印.cpp
+++ 二叉树之字形打印.cpp
@@ -39,3 +39,52 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int> > Print(TreeNode* pRoot) {
+        vector<vector<int>> ret;
+        if (!pRoot) return ret;
+        queue<TreeNode*> q;
+        q.push(pRoot);
+        int level = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int> ans;
+            while (sz--) {
+                TreeNode *node = q.front();
+                q.pop();
+                ans.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ++level;
+            if (!(level&1)) // 偶数层 反转一下
+                reverse(ans.begin(), ans.end());
+            ret.push_back(ans);
+        }
+        return ret;
+    }
+
+};
+
+
+int main()
+{
+	TreeNode *a = new TreeNode(1);
+	a->left = new TreeNode(2);
+	a->left->left= new TreeNode(3);
+	a->right = new TreeNode(4);
+	a->right->right = new TreeNode(5);
+	Solution s;
+	auto zhourongtu = s.Print(a);
+	for(int i=0; i<zhourongtu.size(); i++){
+		for(int j=0; j<zhourongtu[i].size(); j++){
+			cout << zhourongtu[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
