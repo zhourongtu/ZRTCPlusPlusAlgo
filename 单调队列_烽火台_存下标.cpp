4324ef8202c6472ce5f4d2074fc7ea1d8199be35
zhourongtu <zhourongtu@qq.com>
2020-08-22 10:54:07 +0800

    重新认识了单调栈和单调队列

--- 单调队列_烽火台_存下标.cpp
+++ 单调队列_烽火台_存下标.cpp
@@ -19,6 +19,7 @@ public:
         // 前面M-1个点亮自己的代价，都是自己（其实第M个也是）
         deque<int> single_queue;
         
+        // 维护一个单调递增的栈-->价格最低
         for(int i=1; i<=M; ++i){
             f[i] = cost[i];
             while(!single_queue.empty() && f[single_queue.back()] >= f[i]){
@@ -29,9 +30,8 @@ public:
         // 从第M个开始，（设为i）
         // 代价为 i-M~i-1（因为自己必定点亮）中最小代价+自己代价
         for(int i=M+1; i<=N; ++i){ 
-            f[i] = 0x7FFFFFFF;
             f[i] = f[single_queue.front()] + cost[i]; // 自己的最小代价
-            while(!single_queue.empty() && f[single_queue.back()] >= f[i]){ 
+            while(!single_queue.empty() && f[single_queue.back()] >= f[i]){ // 价值大的全部抛弃
                 single_queue.pop_back();
             }
             single_queue.push_back(i);
@@ -39,7 +39,7 @@ public:
                 这里如果在single_queue中有多个值，更大的值如果在前面的下标，此时没有被pop掉
             */
             // 实际上可以用if
-            while(!single_queue.empty() && single_queue.front() < i-M+1) {
+            while(!single_queue.empty() && single_queue.front() < i-M+1) {// 对下一次而言，无效的全部抛弃
                 single_queue.pop_front();
             }
         }
