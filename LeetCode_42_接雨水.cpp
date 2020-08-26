4141476f86218450d2dc43e6de52fee4f6d83c43
zhourongtu <zhourongtu@qq.com>
2020-08-26 18:38:23 +0800

    添加了LRU缓存机制

--- LeetCode_42_接雨水.cpp
+++ LeetCode_42_接雨水.cpp
@@ -6,6 +6,12 @@
 #include <stack>
 using namespace std;
 
+// 其他解法
+// 核心：1.暴力：查找这个点左右的最高点，该点可以存储的长度。
+// 每次都要查找-->存储下来。2.遍历两次，分别存储该点左侧最高和右侧最高。
+// 3.利用栈-->一次就能获取长度。
+// 4.
+
 /*请在这里输入这个程序的功能或者作用*/
 class Solution {
 public:
@@ -77,6 +83,32 @@ public:
         return max_water;
     }
 };
+
+// 方法3：利用栈
+/*
+class Solution{
+public:
+    int trap(vector<int>& height)
+    {
+        int ans = 0, current = 0;
+        stack<int> st;
+        while (current < height.size()) {
+            while (!st.empty() && height[current] > height[st.top()]) {
+                int top = st.top();
+                st.pop();
+                if (st.empty())
+                    break;
+                int distance = current - st.top() - 1;
+                int bounded_height = min(height[current], height[st.top()]) - height[top];
+                ans += distance * bounded_height;
+            }
+            st.push(current++);
+        }
+        return ans;
+    }
+
+};
+*/
 int main()
 {
     vector<int> nums = {0,1,0,2,1,0,1,3,2,1,2,1};
